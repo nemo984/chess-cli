94635c101a48514764658fa5df18a439a510c115
Moe Thant Kyaw <moethantk@gmail.com>
2021-12-16 16:05:17 +0700

    setup

---
+++ cmd/root.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "chess-cli",
+	Short: "Play chess against computer inside your terminal",
+	Long: `Chess-cli is a CLI to play chess against a computer with various levels of difficulty
+	
+	`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.chess-cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/nemo984/chess-cli
+
+go 1.17
+
+require github.com/spf13/cobra v1.3.0
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
