2e1cb24f1e4aefac1daed5fe0fe1d782588d0fc5
Moe Thant Kyaw <moethantk@gmail.com>
2021-12-16 16:19:49 +0700

    add data.go

--- data/data.go
+++ data/data.go
@@ -1,1 +1,40 @@
 package data
+
+import (
+	"database/sql"
+	"log"
+)
+
+var db *sql.DB;
+
+func OpenDatabase() error {
+	var err error;
+
+	db, err = sql.Open("sqlite3", "./sqlite-database.db")
+	if err != nil {
+		return err
+	}
+	return db.Ping()
+}
+
+func CreateTable() {
+	createTableSQL := `CREATE TABLE IF NOT EXISTS games(
+		"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"color" TEXT,
+		"engine" TEXT,
+		"computerColor"	TEXT,
+		"level" INTEGER,
+		"isWon" BOOLEAN,
+		"isCheckmate" BOOLEAN,
+		"isStalemate" BOOLEAN,
+		"pgn" TEXT,
+	);`
+
+	statement, err :=  db.Prepare(createTableSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	statement.Exec()
+	log.Println("Games table created")
+}
--- main.go
+++ main.go
@@ -4,8 +4,12 @@ Copyright © 2021 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/nemo984/chess-cli/cmd"
+import (
+	"github.com/nemo984/chess-cli/cmd"
+	"github.com/nemo984/chess-cli/data"
+)
 
 func main() {
+	data.OpenDatabase();
 	cmd.Execute()
 }
